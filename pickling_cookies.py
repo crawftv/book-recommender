5a072f367405097889d140eeeb9a78be26314725
Crawford <crawford.collins@gmail.com>
2020-02-22 23:28:36 -0600

    added pickling_cookies.py

---
+++ pickling_cookies.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import pickle
+
+def pickle_thing(obj, filename):
+   with open(filename, "wb") as outfile:
+       pickle.dump(obj, filename)
+       outfile.close()
+
+def unpickle_thing(filename):
+    with open(filename,"rb") as infile:
+        obj = pickle.load(infile)
+        infile.close()
+        return obj
