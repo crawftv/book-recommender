2c940708a12f9cfcc125b60620156a4948abe27e
Crawford Collins <crawford.collins@gmail.com>
2020-03-08 12:54:03 -0500

    initial work done

--- pickling_cookies.py
+++ pickling_cookies.py
@@ -3,9 +3,9 @@
 import pickle
 
 def pickle_thing(obj, filename):
-   with open(filename, "wb") as outfile:
-       pickle.dump(obj, filename)
-       outfile.close()
+    with open(filename, "wb") as outfile:
+        pickle.dump(obj, outfile)
+        outfile.close()
 
 def unpickle_thing(filename):
     with open(filename,"rb") as infile:
